9c1d3c61e1292e9acaaf27271af2575f6f8504fa
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-10 02:54:15 +0000

    Add unittests for testing created_at and updated_up instances.

--- product/tests/test_models.py
+++ product/tests/test_models.py
@@ -5,7 +5,7 @@ from ..models.Product_Models import Product
 class TestProductModels(TestCase):
 
     def setUp(self):
-        self.product = Product.objects.create(
+        self.product = Product(
             product_name='testing_product',
             product_description='This is a testing case.',
             product_price=100.0
@@ -25,3 +25,56 @@ class TestProductModels(TestCase):
 
     def test_str_repr_method_returns_the_product_name(self):
         self.assertEqual(str(self.product), self.product.product_name)
+
+    def test_created_at_is_set_on_create(self):
+        product = Product(
+            product_name='testing',
+            product_slug='testing',
+            product_description='This is a testing',
+            product_price=100.0
+        )
+        self.assertIsNone(product.created_at)
+
+        product.save()
+
+        self.assertIsNotNone(product.created_at)
+
+    def test_updated_at_is_set_on_create(self):
+        product = Product(
+            product_name='testing',
+            product_slug='testing',
+            product_description='This is a testing',
+            product_price=100.0
+        )
+
+        self.assertIsNone(product.updated_at)
+
+        product.save()
+
+        self.assertIsNotNone(product.updated_at)
+
+    def test_updated_at_is_set_on_update(self):
+        product = Product(
+            product_name='testing',
+            product_slug='testing',
+            product_description='This is a testing',
+            product_price=100.0
+        )
+        product.save()
+        updated_at = product.updated_at
+        product.save()
+
+        self.assertNotEquals(updated_at, product.updated_at)
+
+    def test_created_at_is_not_set_on_update(self):
+        product = Product(
+            product_name='testing',
+            product_slug='testing',
+            product_description='This is a testing',
+            product_price=100.0
+        )
+        product.save()
+        created_at = product.created_at
+    
+        product.save()
+        self.assertEquals(created_at, product.created_at)
