164d0baf61ba12026d18503796ed01382a337ef2
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-10 03:49:08 +0000

    Add category_id to the Product model.

--- product/models/product.py
+++ product/models/product.py
@@ -8,9 +8,13 @@ class Product(models.Model):
     product_name = models.CharField(max_length=254,)
     product_sku = models.CharField(max_length=254, null=True, blank=True)
     product_slug = models.SlugField(max_length=254, null=False, unique=True)
+
     product_description = models.TextField()
     product_price = models.DecimalField(max_digits=6, decimal_places=2)
     product_image = models.ImageField(null=True, blank=True)
+    
+    category_id = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now = True)
     deleted_at = models.DateTimeField(null = True)
