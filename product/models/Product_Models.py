6d440fd981da7cfe834596fc3c19a7b0ba4103ba
Emidio Valeretto <emidio.valereto@gmail.com>
2022-10-10 02:55:24 +0000

    Add created_at and updated_up attrs.

--- product/models/Product_Models.py
+++ product/models/Product_Models.py
@@ -11,10 +11,15 @@ class Product(models.Model):
     product_description = models.TextField()
     product_price = models.DecimalField(max_digits=6, decimal_places=2)
     product_image = models.ImageField(null=True, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now = True)
+    deleted_at = models.DateTimeField(null = True)
+
+    class Meta:
+        ordering = ['-created_at']
 
     def __str__(self):
         """
         This method returns the string representation of the object.
         """
         return self.product_name
-    
